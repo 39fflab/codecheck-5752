531d59ddf3643b49a1fe6854cb3b4c2abfd6f5cb
39fflab <39ff2015+lab@gmail.com>
2017-01-24 22:08:33 +0900

    Update hello.js

--- app/hello.js
+++ app/hello.js
@@ -4,7 +4,8 @@ function hello(name) {
 * nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
 */
   //ここにHelloworldを実装します。
+  if(name === "codecheck"){
+  	return "Hello codecheck!";
+  }
   return "Hello World!";
 }
-
-module.exports = hello;
